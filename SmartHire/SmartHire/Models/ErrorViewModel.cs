c545d60c666ccbfe8f9bdd3dab380f249d543873
ghazal kh <ghazalkhaleel1@gmail.com>
2026-04-08 14:45:21 +0300

    ok

--- SmartHire/SmartHire/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartHire.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
