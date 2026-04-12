23dd4fa9227c5491825153105de95c086a113f0c
OMARbا <omaralbayyari00@gmail.com>
2026-04-12 21:48:14 +0300

    Complete setup for Database connection, DbContext, and AccountController

---
+++ Employment/Employment/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employment.Data;
+using Employment.Models;
+using System.Linq;
+
+namespace Employment.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+     
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+     
+    }
+}
