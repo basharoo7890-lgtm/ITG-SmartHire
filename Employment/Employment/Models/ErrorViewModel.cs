af644a263935dcc9399bf1d8ec290f9e21ed776e
OMARbا <omaralbayyari00@gmail.com>
2026-04-29 18:25:37 +0300

    Ignore system and temp files

--- Employment/Employment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Employment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
